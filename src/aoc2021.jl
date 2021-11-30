d2f0439163c213d0ffe42d655b69658fd94d0997
Nick Williams <ntwilliams.personal@gmail.com>
2021-11-30 11:57:16 -0800

    Setup

--- src/aoc2021.jl
+++ src/aoc2021.jl
@@ -1,5 +1,25 @@
 module aoc2021
 
-greet() = print("Hello World!")
+using Printf
 
-end # module
+# All inspired by https://github.com/goggle/AdventOfCode2021.jl
+
+solvedDays = readdir(joinpath(@__DIR__, "days"))
+
+# Include the source files:
+for day in solvedDays
+    include(joinpath(@__DIR__, "days", day))
+end
+
+# Read the input from a file:
+function readInput(day::Integer)
+    path = joinpath(@__DIR__, "..", "data", @sprintf("%02d.txt", day))
+    input = open(path, "r") do file
+        read(file, String)
+    end
+    return input
+end
+
+export readInput
+
+end
